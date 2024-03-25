1138dcf56e1e7db0375688fabea241883f6d7884
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-03-25 17:45:23 +0100

    Add error handling utility function

---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express'
+import { isHttpError } from 'http-errors'
+
+const errorHandler = (
+  error: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log(error)
+  let errorMessage = 'unknown error has occurred'
+  let statusCode = 500
+  if (isHttpError(error)) {
+    statusCode = error.status
+    errorMessage = error.message
+  }
+  res.status(statusCode).json({ message: errorMessage })
+}
+
+export default errorHandler
