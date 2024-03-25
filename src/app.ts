407183efb5841e5c1c2b0e7fda80582d9d48a97f
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-03-25 17:45:32 +0100

    Add CORS, logging, and error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+import cors from 'cors'
 import express from 'express'
+import createHttpError from 'http-errors'
+import morgan from 'morgan'
+import errorHandler from './utils/errorHandler'
 
-export const app = express()
+const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+
+app.use((req, res, next) => {
+  next(createHttpError(404, 'Endpoint not found'))
+})
+
+app.use(errorHandler)
+
+export default app
