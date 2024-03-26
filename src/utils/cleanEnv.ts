edaef7376fef7f67cd84f937ebccffe68b5b23ce
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-03-26 15:45:16 +0100

    Add access and refresh token secrets to environment variables

--- src/utils/cleanEnv.ts
+++ src/utils/cleanEnv.ts
@@ -3,4 +3,6 @@ import { cleanEnv, port, str } from 'envalid'
 export default cleanEnv(process.env, {
   PORT: port(),
   MONGO_URI: str(),
+  ACCESS_TOKEN_SECRET: str(),
+  REFRESH_TOKEN_SECRET: str(),
 })
