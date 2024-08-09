a5460dad34b1ae2af8a6e4c95a5fd20ac0f23847
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-08-09 20:19:25 +0200

    fix type for new password in request type

--- @types/express.d.ts
+++ @types/express.d.ts
@@ -1,11 +1,12 @@
 import { Request } from "express";
+
 declare global {
    namespace Express {
       export interface Request {
          user: {
             _id: string;
             username: string;
-            password: string;
+            password: string | null | undefined;
             googleId?: string | null;
             githubId?: string | null;
             picture?: string | null;
